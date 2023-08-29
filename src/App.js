ecbc3875d13274297c37567489c6431b327bdba5
Abhishek Adhikary <abhishek.112031@gmail.com>
2023-08-29 20:26:20 +0530

    first commit:adding Expense  items inside comonent

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ExpenseComponent from './components/ExpenseCpmponent'
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseComponent></ExpenseComponent>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseCpmponent.js
@@ -0,0 +1,32 @@
+
+import './ExpenseComponent.css'
+function ExpenseComponent() {
+    return (
+        <div>
+            <h2>Expense item!</h2>
+            <div className="items">
+                <div>Food</div>
+                <div>RS: 10</div>
+            </div>
+            <div className="items">
+                <div>Petrol</div>
+                <div>RS: 100</div>
+            </div>
+            <div className="items">
+                <div>Movies</div>
+                <div>RS: 200</div>
+            </div>
+
+
+        </div>
+    )
+}
+export default ExpenseComponent;
+
+//Expense Items
+
+// Food Rs 10
+
+// Petrol Rs 100
+
+// Movies Rs 200
