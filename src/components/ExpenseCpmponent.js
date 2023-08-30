6cbe90fd6bddde5281cc20f59002c74a83155480
Abhishek Adhikary <abhishek.112031@gmail.com>
2023-08-30 08:28:44 +0530

    adding location section

--- src/components/ExpenseCpmponent.js
+++ src/components/ExpenseCpmponent.js
@@ -4,11 +4,13 @@ function ExpenseComponent() {
     const expenseDate=new Date(2023,7,30);
     const expenseTitle='Petrol';
     const expenseAmount=100;
+    const LocationOfExpenditure='Delhi'
     return (
         <div className='expense-item'>
             <div>{expenseDate.toDateString()}</div>
           
             <div className="expense-item__description">
+                <p className='location'>Location: {LocationOfExpenditure}</p>
                 <h2>{expenseTitle}</h2>
                 <div className='expense-item__price'>RS: {expenseAmount}</div>
             </div>
