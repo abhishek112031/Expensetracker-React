86c2147fbd94178aee0659d44abfef737bc6e51a
Abhishek Adhikary <abhishek.112031@gmail.com>
2023-08-30 10:46:16 +0530

    adding location and add some styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import ExpenseComponent from './components/ExpenseCpmponent'
 
 function App() {
+  const expenses= [
+    {
+      id: 1,
+      title: "Item 1",
+      amount: 10.99,
+      LocationOfExpenditure:'delhi',
+      date: new Date()
+    },
+    {
+      id: 2,
+      title: "Item 2",
+      amount: 25.49,
+      LocationOfExpenditure:'kharagpur',
+      date: new Date()
+    },
+    {
+      id: 3,
+      title: "Item 3",
+      amount: 5.0,
+      LocationOfExpenditure:'mumbai',
+      date:new Date()
+    },
+    {
+      id: 4,
+      title: "Item 4",
+      amount: 15.0,
+      LocationOfExpenditure:'hydrabad',
+      date: new Date()
+    }
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseComponent></ExpenseComponent>
+      <ExpenseComponent title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} LocationOfExpenditure={expenses[0].LocationOfExpenditure}></ExpenseComponent>
+   
     </div>
   );
 }
--- src/components/ExpenseCpmponent.js
+++ src/components/ExpenseCpmponent.js
@@ -1,18 +1,26 @@
 
 import './ExpenseComponent.css'
-function ExpenseComponent() {
-    const expenseDate=new Date(2023,7,30);
-    const expenseTitle='Petrol';
-    const expenseAmount=100;
-    const LocationOfExpenditure='Delhi'
+function ExpenseComponent(props) {
+
+    const month=props.date.toLocaleString('en-us',{month:'long'})
+    const day=props.date.toLocaleString('en-us',{day:'2-digit'});
+    const year=props.date.getFullYear();
+
+
+   
     return (
         <div className='expense-item'>
-            <div>{expenseDate.toDateString()}</div>
+            <div>
+                <div>{month}</div>
+                <div>{day}</div>
+                <div>{year}</div>
+
+            </div>
           
             <div className="expense-item__description">
-                <p className='location'>Location: {LocationOfExpenditure}</p>
-                <h2>{expenseTitle}</h2>
-                <div className='expense-item__price'>RS: {expenseAmount}</div>
+                <p className='location'>Location: {props.LocationOfExpenditure}</p>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>RS: {props.amount}</div>
             </div>
 
         </div>
