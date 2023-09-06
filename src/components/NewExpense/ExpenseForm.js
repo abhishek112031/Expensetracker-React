4fe0d67142da5d55023b652c0c5c10c65ff9a223
Abhishek Adhikary <abhishek.112031@gmail.com>
2023-09-06 17:15:16 +0530

    define handeller for other inputs

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+    //state:
+    const [enteredTitle,setEnteredTitle]=useState('');
+    const [enteredAmount,setEnteredAmount]=useState('');
+    const [enteredDate,setEnteredDate]=useState('');
+    
+    // handellers:
     const titleChangeHandeller=(event)=>{
-        console.log(event.target.value)
+        setEnteredTitle(event.target.value);
     }
+    const amountChangeHandeller=(event)=>{
+        setEnteredAmount(event.target.value)
+    }
+    const dateChangeHandeller=(event)=>{
+        setEnteredDate(event.target.value);
+    }
+    
     return (
         <form>
             <div className='new-expense__control'>
@@ -14,11 +27,11 @@ const ExpenseForm = () => {
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type='number' min='0.01' step='0.01' />
+                    <input type='number' min='0.01' step='0.01' onChange={amountChangeHandeller} />
                 </div>
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type='date' min='2019-01-01' step='2023-01-01' />
+                    <input type='date' min='2019-01-01' step='2023-01-01' onChange={dateChangeHandeller} />
                 </div>
             </div>
             <div className='new-expense__actions'>
